2e814012641ea3152a4b0d8065f4047e32747c65
LucaMac1 <macchiusiluca@gmail.com>
2020-12-16 23:37:21 +0000

    Hello World second line

--- hello.cpp
+++ hello.cpp
@@ -3,7 +3,8 @@
 
 int main(int arc, char **arv)
 {
-    std::cout << "Hello Wordld!" << std::endl;
+    std::cout << "Hello Wordld first line!" << std::endl;
+    std::cout << "Hello Wordld second line!" << std::endl;
     std::cin.get();
 
     return EXIT_SUCCESS;
