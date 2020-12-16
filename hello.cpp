e9c454395e8f296c2a8c294cec749d9f0a655264
LucaMac1 <macchiusiluca@gmail.com>
2020-12-16 23:40:18 +0000

    Restore 1st and 2nd lines

--- hello.cpp
+++ hello.cpp
@@ -3,8 +3,7 @@
 
 int main(int arc, char **arv)
 {
-    std::cout << "Hello Wordld first line!" << std::endl;
-    std::cout << "Hello Wordld second line!" << std::endl;
+    std::cout << "Hello Wordld!" << std::endl;
     std::cin.get();
 
     return EXIT_SUCCESS;
