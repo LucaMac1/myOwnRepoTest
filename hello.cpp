bedc259e84a2778be8193f1e79b97aa07a68d4f4
LucaMac1 <macchiusiluca@gmail.com>
2020-12-26 12:39:38 +0100

    before rebase msg

--- hello.cpp
+++ hello.cpp
@@ -8,7 +8,7 @@ int main(int arc, char **arv)
     std::cout << "Hello you!" << std::endl;    
     std::cout << "Master" << std::endl;
     std::cout << "Experiment"<<std::endl;
-
+    std::cout << "before rebase" << std::endl;
     std::cin.get();
 
     return EXIT_SUCCESS;
