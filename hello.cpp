107f15d9fc9503a2e5ee5270b53f62ff9e4a620c
LucaMac1 <macchiusiluca@gmail.com>
2020-12-26 12:12:30 +0100

    Write Master

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,8 @@ int main(int arc, char **arv)
     std::cout << "Hello Wordld!" << std::endl;
     std::cout << "Hello Mars!" << std::endl;
     std::cout << "Hello you!" << std::endl;
+    
+    std::cout << "Master" << std::endl;:
     std::cin.get();
 
     return EXIT_SUCCESS;
